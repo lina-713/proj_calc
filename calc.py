6d35cf21dd4132c48bbce0c4bc6d0423df37bd26
Kamila <kamila.alexsandrova@bk.ru>
2024-04-13 15:06:28 +0300

    calc interface from kamila

--- calc.py
+++ calc.py
@@ -2,9 +2,10 @@ import tkinter as tk
 from tkinter import messagebox
 import math
 
+
 def calculate():
     operation = operation_var.get()
-    
+
     if operation in ["Sin", "Cos", "Квадратный корень", "Округление в меньшую сторону", "Округление в большую сторону"]:
         num1 = float(entry_num1.get())
         if operation == "Sin":
@@ -17,17 +18,17 @@ def calculate():
             result = math.floor(num1)
         elif operation == "Округление в большую сторону":
             result = math.ceil(num1)
-            
+
         result_label.config(text="Результат: " + str(result))
-        
+
     else:
         num1 = float(entry_num1.get())
         num2 = float(entry_num2.get())
-        
+
         if operation == "Деление" and num2 == 0:
             messagebox.showerror("Ошибка", "Деление на ноль невозможно")
             return
-        
+
         if operation == "Сложение":
             result = num1 + num2
         elif operation == "Вычитание":
@@ -40,8 +41,9 @@ def calculate():
             result = num1 % num2
         elif operation == "Возведение в степень":
             result = num1 ** num2
-        
+
         result_label.config(text="Результат: " + str(result))
+
 
 def show_input_fields():
     operation = operation_var.get()
@@ -52,3 +54,39 @@ def show_input_fields():
         entry_num1.pack()
         entry_num2.pack()
 
+    # Создание основного окна
+
+
+root = tk.Tk()
+root.title("Калькулятор")
+root.geometry("400x300")  # Установка размеров окна
+
+# Выбор операции
+operation_var = tk.StringVar(root)
+operations = [
+    "Сложение", "Вычитание", "Умножение", "Деление",
+    "Остаток от деления", "Sin", "Cos", "Возведение в степень",
+    "Квадратный корень", "Округление в меньшую сторону",
+    "Округление в большую сторону"
+]
+operation_var.set(operations[0])
+operation_menu = tk.OptionMenu(root, operation_var, *operations)
+operation_menu.pack(pady=5)
+
+# Поля для ввода чисел
+entry_num1 = tk.Entry(root)
+entry_num2 = tk.Entry(root)
+
+# Кнопка "Выбор операции"
+select_operation_button = tk.Button(root, text="Выбрать операцию", command=show_input_fields)
+select_operation_button.pack(pady=5)
+
+# Кнопка "Вычислить"
+calculate_button = tk.Button(root, text="Вычислить", command=calculate)
+calculate_button.pack(pady=10)
+
+# Поле для вывода результата
+result_label = tk.Label(root, text="")
+result_label.pack(pady=5)
+
+root.mainloop()
