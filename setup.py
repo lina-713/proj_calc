a2edc761b3d8c5e5b2fd4c8efcd5180294982db6
lina-713 <57530028+lina-713@users.noreply.github.com>
2024-05-24 22:02:23 +0300

    Create setup.py
    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CalculatorApp',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'calculator = calc:main',  
+        ],
+    },
+    install_requires=[
+        
+    ],
+    author='Alina_V, Kamila_A',
+    author_email='alinav2048@gmail.com',
+    description='Simple calculator app',
+    license='MIT',
+    keywords='calculator app',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
